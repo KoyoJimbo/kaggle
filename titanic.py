d7f286288e37d2dd74045ea3f715aac7d92534d7
jmbo <jmbo@jmbo-VirtualBox>
2017-11-17 23:59:32 +0900

    dont dorp sibsp and parch

--- titanic.py
+++ titanic.py
@@ -7,16 +7,17 @@ train_df = pd.read_csv("train.csv", header=0)
 
 # Convert "Sex" to be a dummy variable (female = 0, Male = 1)
 train_df["Gender"] = train_df["Sex"].map({"female": 0, "male": 1}).astype(int)
-train_df.head(3)
+train_df.head(6)
 
 # Complement the missing values of "Age" column with average of "Age"
 median_age = train_df["Age"].dropna().median()
 if len(train_df.Age[train_df.Age.isnull()]) > 0:
   train_df.loc[(train_df.Age.isnull()), "Age"] = median_age
 
+
 # remove un-used columns
-train_df = train_df.drop(["Name", "Ticket", "Sex", "SibSp", "Parch", "Fare", "Cabin", "Embarked", "PassengerId"], axis=1)
-train_df.head(3)
+train_df = train_df.drop(["Name", "Ticket", "Sex", "Fare", "Cabin", "Embarked", "PassengerId"], axis=1)
+train_df.head(6)
 
 # Load test data, Convert "Sex" to be a dummy variable
 test_df = pd.read_csv("test.csv", header=0)
@@ -29,7 +30,7 @@ if len(test_df.Age[test_df.Age.isnull()]) > 0:
 
 # Copy test data's "PassengerId" column, and remove un-used columns
 ids = test_df["PassengerId"].values
-test_df = test_df.drop(["Name", "Ticket", "Sex", "SibSp", "Parch", "Fare", "Cabin", "Embarked", "PassengerId"], axis=1)
+test_df = test_df.drop(["Name", "Ticket", "Sex", "Fare", "Cabin", "Embarked", "PassengerId"], axis=1)
 test_df.head(3)
 
 # Predict with "Random Forest"
